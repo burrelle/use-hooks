66d1910f91cdb096ca230d4db7c9aa3d221e08f6
Evan Burrell <burrelle@oregonstate.edu>
2019-02-12 16:38:44 +0000

    MDX start

---
+++ src/components/StateCounter.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: 0 };
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+  }
+
+  increment() {
+    this.setState({ value: this.state.value + 1 });
+  }
+
+  decrement() {
+    this.setState({ value: this.state.value - 1 });
+  }
+
+  render() {
+    return (
+      <div>
+        <p>{this.state.value}</p>
+        <button onClick={this.increment}>+</button>
+        <button onClick={this.decrement}>-</button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
